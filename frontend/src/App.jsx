20de3a9a9decccf9b4c20316411e952f1f4387fb
Shruti <shrutisdixit16@gmail.com>
2024-10-12 11:31:10 -0400

    Created landing page and synced with head

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,14 +1,15 @@
-/*import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'*/
-import './App.css'
+import React from 'react';
+import "bootstrap/dist/css/bootstrap.min.css";
+import 'bootstrap/dist/js/bootstrap.bundle.min';
+import LandingPage from './LandingPage'
 
-function App() {
-  return (
+function App(){
+  return(
     <div>
-      Hi There from app.jsx
+      <LandingPage />
     </div>
-  )
+  );
 }
+
 export default App;
   
